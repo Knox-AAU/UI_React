e9412d569bd7def01a77550b005332b0bcae2d98
Jacopopsen <58552183+Jacopopsen@users.noreply.github.com>
2021-11-10 19:38:38 +0100

    Changed Styling to correct the sticky elements

--- knox-ui/src/Components/Header.js
+++ knox-ui/src/Components/Header.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import logo from '../Img/logo.svg'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import StickyBox from 'react-sticky-box/dist/esnext'
 
 const Header = props => {
-    return (<Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
-    <Container style={{minHeight: "30px",height:"5vh"}}>
-    <Navbar.Brand href="/home"><img style={{minHeight: "30px",height:"5vh"}} className="img-responsive" src={logo} alt="logo"/></Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="/factchecker">Fact Checking</Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link href="/status">Status</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    return (
+    
+    <StickyBox>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light" style={{padding:"0"}}>
+        <Container style={{height:"50px"}}>
+        <Navbar.Brand href="/home"><img style={{height:"40px"}} className="img-responsive" src={logo} alt="logo"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/factchecker">Fact Checking</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/status">Status</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  </StickyBox>
     )
 }
 
--- knox-ui/src/Components/Home.js
+++ knox-ui/src/Components/Home.js
@@ -18,18 +18,26 @@ const Home = props => {
     return (
     <div style={{display: "flex", width: "100%", alignItems: "stretch"}}>
         <div style={{display: "block", width: "100%"}}>
-        <SearchBar
-            searchText="Enter your search"
-            onClick={onClick}
-        />
-        <Button
-            onClick={() => setOpen(!open)}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-            variant="secondary"
-        >
-            Advanced
-        </Button>
+            
+            <div>
+                <h1 style={{fontSize:"5em"}}>Search Contents</h1>
+                <h2 style={{marginBottom:"1em"}}>It is possible to search between multiple datasets of the toolbox!</h2>
+             </div>
+
+        <StickyBox offsetTop={50}>
+            <SearchBar
+                searchText="Enter your search"
+                onClick={onClick}
+            />
+            <Button
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+                variant="secondary"
+            >
+                Advanced
+            </Button>
+        </StickyBox>
         {searchResults.map(result => {
             return (<div className="search-result">
                 <h1>{result.title}</h1>
@@ -39,13 +47,15 @@ const Home = props => {
         </div>
         <div style={{float: "right"}}>
             <Collapse in={open} dimension="width">
-                <div id="example-collapse-text">
-                    <Card body style={{backgroundColor: "cyan", width: '400px', height: "94vh", position:"sticky"}}>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                        labore wes anderson cred nesciunt sapiente ea proident.
-                    </Card>
-                </div>
+                <StickyBox offsetTop={50}>
+                    <div id="example-collapse-text">
+                        <Card body style={{backgroundColor: "cyan", width: '400px', height: "94vh", position:"sticky"}}>
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                            labore wes anderson cred nesciunt sapiente ea proident.
+                        </Card>
+                    </div>
+                </StickyBox>
             </Collapse>
         </div>
     </div>
