6d60c2bebec68f782893abb86a52dbd8a0d118e7
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2021-11-16 14:18:19 +0100

    server stuff

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const { response } = require('express');
+const fs = require('fs')
+
+
 const app = express();
-const serverPort= 8000
+const serverPort= 8000;
 
 /*
 #################################################################
@@ -15,10 +21,34 @@ const serverPort= 8000
 #################################################################
 #################################################################
 */
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get("/search",(req,res)=>{
+  const searchText = req.query.input
+  const sources = req.query.sources
+  fetch("http://localhost:8081/api/search?input=" + encodeURI(searchText)+"&sources=" + encodeURI(sources))
+    .then(response => response.json())
+    .then(json=>res.json(json))
+    .catch(e=>{
+      res.status=500;
+      res.send(e)
+      console.log(e)
+    })
+})
+
+app.get("/getpdf*", (req,res)=>{
+  const id = req.query.id
+  fetch("http://localhost:8081/api/getpdf?id="+id,)
+    .then(response=>response.body.pipe(res))
+    .catch(e=>{
+      res.status=500;
+      res.send(e)
+      console.log(e)
+    })
+})
+
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
